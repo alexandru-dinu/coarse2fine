eca74b3513aa41b576a1cf203aa9b38b5f12fde3
Alexandru Dinu <alex.dinu07@gmail.com>
2019-05-21 19:42:55 +0300

    Better test single.

--- run_test_single.sh
+++ run_test_single.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-dataset=$1
-model_path=$2
+model_path=$1
 
 base_dir=$(dirname "$(readlink -f $0)")
 data_dir=${base_dir}/data_model/${dataset}
 
 python src/test_single.py \
     -root_dir ${base_dir}/data_model/ \
-    -dataset ${dataset} \
     -split test \
-    -batch_size 2 \
+    -batch_size 1 \
     -model_path ${model_path}
--- src/test_single.py
+++ src/test_single.py
@@ -1,19 +1,24 @@
 from __future__ import division
 
 import argparse
+import logging
+from pprint import pprint
 
+import coloredlogs
 import torch
 
 import options
 import table
 import table.IO
-from pprint import pprint
 
 arg_parser = argparse.ArgumentParser()
 options.set_common_options(arg_parser)
 options.set_model_options(arg_parser)
 options.set_translation_options(arg_parser)
 args = arg_parser.parse_args()
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')
 
 if args.cuda:
     torch.cuda.set_device(args.gpu_id[0])
@@ -23,17 +28,24 @@ if args.beam_size > 0:
 
 
 def main():
+    # ex = {
+    #     "token": ["self", ".", "error", "(", "self", ".", "cmd", ".", "missing_args_message", ")"],
+    #     "src"  : ["call", "the", "method", "self.error", "[", "self", ".", "error", "]", "with", "an", "argument", "self.cmd.missing_args_message", "[", "self", ".", "cmd", ".",
+    #               "missing_args_message", "]"],
+    #     "type" : ["self", ".", "FUNC#1", "(", "self", ".", "NAME", ".", "NAME", ")"]
+    # }
+
     ex = {
-        "token": ["self", ".", "error", "(", "self", ".", "cmd", ".", "missing_args_message", ")"],
-        "src"  : ["call", "the", "method", "self.error", "[", "self", ".", "error", "]", "with", "an", "argument", "self.cmd.missing_args_message", "[", "self", ".", "cmd", ".",
-                  "missing_args_message", "]"],
-        "type" : ["self", ".", "FUNC#1", "(", "self", ".", "NAME", ".", "NAME", ")"]
+        'token': "ajkk = dict ( )".split(),
+        'src'  : "ajkk is an empty dict".split(),
+        'type' : "NAME = dict ( )".split()
     }
+
     js_list = [
         table.IO.preprocess_json(ex)
     ]
 
-    metric_name_list = ['tgt']
+    metric_name_list = ['lay-token', 'lay', 'tgt-token', 'tgt']
 
     args.model = args.model_path  # TODO??
     translator = table.Translator(args)
@@ -44,11 +56,10 @@ def main():
         batch_size=args.batch_size, train=False, sort=True, sort_within_batch=False
     )
 
-    print("inference")
     r_list = []
     for i, batch in enumerate(test_data):
         r = translator.translate(batch)
-        pprint(r[0])
+        logger.info(r[0])
         r_list += r
 
     r_list.sort(key=lambda x: x.idx)
@@ -58,11 +69,24 @@ def main():
     for pred, gold in zip(r_list, js_list):
         pred.eval(gold)
 
-    print('Results:')
     for metric_name in metric_name_list:
-        c_correct = sum((x.correct[metric_name] for x in r_list))
-        acc = c_correct / len(r_list)
-        print('{}: {} / {} = {:.2%}'.format(metric_name, c_correct, len(r_list), acc))
+        if metric_name.endswith("-token"):
+            c_correct = sum([len(set(x.get_by_name(metric_name)) - set(y[metric_name.split("-")[0]])) == 0 for x, y in zip(r_list, js_list)])
+            acc = c_correct / len(r_list)
+
+            out_str = '{}: {} / {} = {:.2%}'.format(metric_name.upper(), c_correct, len(r_list), acc)
+            logger.info(out_str)
+
+        else:
+            c_correct = sum((x.correct[metric_name] for x in r_list))
+            acc = c_correct / len(r_list)
+
+            out_str = '{}: {} / {} = {:.2%}'.format(metric_name.upper(), c_correct, len(r_list), acc)
+            logger.info(out_str)
+
+            for x in r_list:
+                for prd, tgt in x.incorrect[metric_name]:
+                    logger.warning("\nprd: %s\ntgt: %s" % (" ".join(prd), " ".join(tgt)))
 
 
 if __name__ == "__main__":
